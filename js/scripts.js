ab8aa213e966f6686fb46162e78b4cfcd1fa4592
DmitrijsSemjonovsDP13 <dmitrijs.semjonovs.dp13@inbox.lv>
2021-04-27 12:04:35 +0300

    viss salauzas (

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,37 @@
+const POP_UP = document.getElementById('popUp');
+let pirkums = []
+
 document.getElementById('pievinotPirkumu').addEventListener('click', () => {
-    console.log("Poga nospiesta!");
+   
+    POP_UP.style.display = 'block';
 
-    let popUp = document.getElementById('popUp')
-
-    popUp.style.display = 'block'
 })
 
 document.getElementById('jaunsPirkumus').addEventListener('click' , () => {
-    let popUp = document.getElementById('popUp')
-
-    popUp.style.display = 'none'
+    POP_UP.style.display = 'none';
     
-    console.log('Poga pievinot pirkumu!')
+    let saraksts = {nosaukums: nosaukums.value, daudzums: daudzums.value};
 
+    nosaukums.value = "";
+    daudzums.value = "";
 
+    pirkums.push(saraksts);
+
+    render();
 })
+
+
+function render() {
+    let pirkumagrozs = document.getElementById('pirkumagrozs');
+
+    for(let i = 0; i < pirkums.length; i++){
+       let groza = `            
+        <div class="groza">
+            <h3>Nosaukums : ${pirkums[i].nosaukums}</h4>
+            <h4>Daudzums: ${pirkums[i].daudzums}</h4> 
+        </div>`;
+    pirkumagrozs.innerHTML += groza;
+    }
+    
+
+}
