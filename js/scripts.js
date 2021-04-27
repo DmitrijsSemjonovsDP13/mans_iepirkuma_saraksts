eb000b9326a53532719592e94b78c02deccfc903
DmitrijsSemjonovsDP13 <dmitrijs.semjonovs.dp13@inbox.lv>
2021-04-27 10:56:03 +0300

    Pirkuma pivinošana poga

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,13 @@ document.getElementById('pievinotPirkumu').addEventListener('click', () => {
 
     popUp.style.display = 'block'
 })
+
+document.getElementById('jaunsPirkumus').addEventListener('click' , () => {
+    let popUp = document.getElementById('popUp')
+
+    popUp.style.display = 'none'
+    
+    console.log('Poga pievinot pirkumu!')
+
+
+})
